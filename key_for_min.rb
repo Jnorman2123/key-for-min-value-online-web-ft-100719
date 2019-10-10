ad057977e52fce4c51574294e722446089e8ba2b
Jonathan Norman <jnorman2123@gmail.com>
2019-10-10 16:07:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil
-  lowest_value = age
+  lowest_value = nil
     name_hash.each do |name, age|
     if age < lowest_value
       lowest_value = age
